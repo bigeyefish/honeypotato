61df46f175f6353c4e2ab62f5e96c1564df40872
王士郁 <shiyu.wang@upchina.com>
2018-04-13 17:51:01 +0800

    remove ICommonService

--- common/src/main/java/com/honeypotato/common/iservice/order/IOrderService.java
+++ common/src/main/java/com/honeypotato/common/iservice/order/IOrderService.java
@@ -1,12 +1,11 @@
 package com.honeypotato.common.iservice.order;
 
 import com.honeypotato.common.dto.order.Order;
-import com.honeypotato.common.iservice.ICommonService;
 
 /**
  * Created by sanyihwang on 2018/4/12.
  */
-public interface IOrderService extends ICommonService<Order>{
+public interface IOrderService {
 
     /**
      * 新增或更新记录
